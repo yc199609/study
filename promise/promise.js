671ab971b56cf36d2d33d48ef51f4079b962f8aa
yc <3095220780@qq.com>
2020-04-26 22:49:13 +0800

    p30

--- promise/promise.js
+++ promise/promise.js
@@ -51,44 +51,64 @@
         }
     }
 
+    // 返回一个promise对象 promise的结果由onResolved/onRejected执行结果决定
+    // 指定成功和失败的回调函数
     Promise.prototype.then = function(onResolved, onRejected) {
+        onResolved = typeof onResolved === 'function' ? onResolved : value => value
+
+        // 指定默认的失败回调(实现错误/异常穿透的关键点)
+        onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason}
+
         const self = this
         
         // 返回一个新的promise对象
         return new Promise((resolve, reject) => {
+
+            // 调用指定的回调函数处理
+            function handle(callback) {
+                // 如果抛出异常，return的promise会失败，reason是error
+                // 如果回调函数执行返回 非promise， return的promise会成功，value是返回的值
+                // 如果返回的函数是promise，return的promise的结果就是这个promise
+                try {
+                    const result = callback(self.data)
+                    if(result instanceof Promise) {
+                        // result.then(
+                        //     value => resolve(value),
+                        //     reason => reject(reason)
+                        // )
+                        result.then(resolve, reject)
+                    } else {
+                        resolve(result)
+                    }
+                } catch (e){
+                    reject(e)
+                }
+            }
+
             if(self.status === PENDING) {
                 // 将回调函数保存起来
-                self.callbacks.push({onResolved, onRejected})
+                self.callbacks.push({
+                    onResolved (value) {
+                        handle(onResolved)
+                    }, 
+                    onRejected (reason) {
+                        handle(onRejected)
+                    }
+                })
             } else if(self.status === RESOLVED) {
                 setTimeout(() => {
-                    // 如果抛出异常，return的promise会失败，reason是error
-                    // 如果回调函数执行返回 非promise， return的promise会成功，value是返回的值
-                    // 如果返回的函数是promise，return的promise的结果就是这个promise
-                    try {
-                        const result = onResolved(self.data)
-                        if(result instanceof Promise) {
-                            result.then(
-                                value => resolve(value),
-                                reason => reject(reason)
-                            )
-                        } else {
-                            resolve(result)
-                        }
-                    } catch (e){
-                        reject(e)
-                    }
-                    
+                    handle(onResolved)
                 })
             } else if(self.status === REJECTED) {
                 setTimeout(() => {
-                    onRejected(self.data)
+                    handle(onRejected)
                 })
             }
         })
     }
 
     Promise.prototype.catch = function(onRejected) {
-
+        return this.then(undefined, onRejected)
     }
 
     Promise.all = function(promises) {
@@ -100,12 +120,22 @@
     }
 
     Promise.resolve = function(value) {
-
+        // 返回一个promise
+        return new Promise((resolve, reject) => {
+            if(value instanceof Promise) {
+                value.then(resolve, reject)
+            } else {
+                resolve(value)
+            }
+        })
     }
-
+ 
     Promise.reject = function(reason) {
-
-    }
+        // 返回一个失败的promise
+        return new Promise((resolve, reject) => {
+            reject(reason)
+        })
+    } 
 
     window.Promise = Promise
 })(window)
